dc15f6fce911b3f640262981590bc6fb958156c3
jbuckner <jon@redbonesolutions.com>
2016-11-11 21:36:22 -0500

    Testing

--- src/test/java/com/redbonesolutions/HighlineApplicationTests.java
+++ src/test/java/com/redbonesolutions/HighlineApplicationTests.java
@@ -1,12 +1,13 @@
 package com.redbonesolutions;
 
+import com.redbonesolutions.highline.HighlineApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = HighlineApplication.class)
 public class HighlineApplicationTests {
 
 	@Test
